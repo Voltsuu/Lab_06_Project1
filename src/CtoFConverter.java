cd18f43a31284e902b9dffc1948daae14c428d19
Voltsuuu <npoudel2007@gmail.com>
2023-10-11 09:32:56 -0400

    Lab_06_Project1

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,5 +1,27 @@
-public class Main {
+import java.util.Scanner;
+
+public class CtoFConverter {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        double tempC = 0.00;
+        double tempF = 0.00;
+        boolean valid = false;
+
+        System.out.println("Please input your temperature in Celsius.");
+        do {
+            if (scan.hasNextDouble()) {
+                valid = true;
+            }
+            else{
+                scan.nextLine();
+                System.out.println("Invalid input, please try again");
+            }
+        } while (!valid) ;
+        tempC = scan.nextDouble();
+        tempF = tempC * 9 / 5 + 32;
+
+        System.out.printf("%-10s%7.2f", "Degrees C:", tempC);
+        System.out.printf("\n%-10s%7.2f", "Degrees F", tempF);
+
     }
 }
